a59f7fe3c9faf29e9b82b5d4d2161a30fa302d39
Emmanuelomolaju <emmanuelomolaju964@gmail.com>
2025-01-06 22:23:31 +0100

    add: navbar (in progress)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -21,22 +21,22 @@ const Header = (props: {
               e.stopPropagation();
               props.setSidebarOpen(!props.sidebarOpen);
             }}
-            className="z-99999 block rounded-sm border border-stroke bg-white p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark lg:hidden"
+            className="z-99999 block rounded-sm border p-1.5 lg:hidden"
           >
             <span className="relative block h-5.5 w-5.5 cursor-pointer">
               <span className="du-block absolute right-0 h-full w-full">
                 <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white ${
+                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-white delay-[0] duration-200 ease-in-out dark:bg-white ${
                     !props.sidebarOpen && '!w-full delay-300'
                   }`}
                 ></span>
                 <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white ${
+                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-white delay-150 duration-200 ease-in-out dark:bg-white ${
                     !props.sidebarOpen && 'delay-400 !w-full'
                   }`}
                 ></span>
                 <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white ${
+                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-white delay-200 duration-200 ease-in-out dark:bg-white ${
                     !props.sidebarOpen && '!w-full delay-500'
                   }`}
                 ></span>
@@ -71,14 +71,14 @@ const Header = (props: {
         <div className="flex items-center gap-3 2xsm:gap-7">
           <div className="hidden sm:block">
             <form action="https://formbold.com/s/unique_form_id" method="POST">
-              <div className="bg-slate-400 p-2 flex gap-3 items-center rounded-3xl">
+              <div className="bg-secondary border border-[#E2E8F04D] p-2 flex gap-3 items-center rounded-3xl">
                 <button className="">
-                  <CiSearch className="text-black dark:text-white" />
+                  <CiSearch className="text-white" />
                 </button>
 
                 <input
                   type="text"
-                  placeholder="Type to search..."
+                  placeholder="Search datasets..."
                   className="w-full bg-transparent focus:outline-none text-white xl:w-125"
                 />
               </div>
